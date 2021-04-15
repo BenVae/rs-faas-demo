a533cd3040bcafc9379a06459d8eab66aa952ce0
Robert Nickel <robert.nickel@aikq.de>
2021-04-15 19:27:25 +0200

    /start sends the latest image with caption to the user. Every other text triggers "... isn't martian" message.

--- src/main/scala/faas/ApiHandler.scala
+++ src/main/scala/faas/ApiHandler.scala
@@ -12,6 +12,7 @@ import com.amazonaws.regions.Regions;
 import com.amazonaws.services.dynamodbv2._
 import com.amazonaws.services.dynamodbv2.model.AttributeValue
 import scala.collection.JavaConverters._
+import com.amazonaws.services.dynamodbv2.model.PutItemResult
 
 object ApiHandler {
   val token = System.getenv("BOT_TOKEN")
@@ -19,6 +20,8 @@ object ApiHandler {
     .standard()
     .withRegion(Regions.EU_CENTRAL_1)
     .build();
+  val backend = HttpURLConnectionBackend()
+  val BASE_URL = s"https://api.telegram.org/bot$token"
 
   def handle(
       event: ScalaApiGatewayEvent,
@@ -29,22 +32,16 @@ object ApiHandler {
         ScalaResponse("error: " + error.getMessage(), statusCode = 404)
       }
       case Right(update) => {
-        if (update.message.text == "/start") {
-          val firstImage = readImage("1").get("prev").get.getS()
-          putUser(update.message.chat.id, firstImage)
-
-          val request = basicRequest
-            .body(
-              Map(
-                "chat_id" -> update.message.chat.id.toString,
-                "text" -> (firstImage)
-              )
-            )
-            .post(uri"https://api.telegram.org/bot$token/sendMessage")
-          val backend = HttpURLConnectionBackend()
-          val response = request.send(backend)
+        val userId = update.message.chat.id
+        val text = update.message.text
+        if (text == "/start") {
+          val firstImageId = readImage("1").get("prev").get.getS()
+          val firstImage = readImage(firstImageId)
+          sendImage(userId, firstImage)
+          putUser(userId, firstImageId)
+        } else {
+          sendMessage(userId, text + " isn\'t martian.")
         }
-
         ScalaResponse("OK")
       }
     }
@@ -64,7 +61,7 @@ object ApiHandler {
       .toMap
   }
 
-  def putUser(id: Int, currentImage: String) = {
+  def putUser(id: Int, currentImage: String): PutItemResult = {
     client
       .putItem(
         "mars_users", {
@@ -80,6 +77,54 @@ object ApiHandler {
         }
       )
   }
+
+  // TODO: Find out the return type
+  def sendMessage(chatId: Int, text: String) = {
+    basicRequest
+      .body(
+        Map(
+          "chat_id" -> chatId.toString,
+          "text" -> text
+        )
+      )
+      .post(uri"$BASE_URL/sendMessage")
+      .send(backend)
+  }
+
+  // TODO: Find out the return type
+  def sendImage(chatId: Int, image: Map[String, AttributeValue]) = {
+    val url = image.get("url").get.getS()
+    val partialRequestForm = Map(
+      "chat_id" -> chatId.toString,
+      "caption" -> getCaption(image)
+    )
+    val (command, requestForm) =
+      if (isPhoto(url)) {
+        ("sendPhoto", partialRequestForm + ("photo" -> url))
+      } else {
+        ("sendAnimation", partialRequestForm + ("animation" -> url))
+      }
+
+    basicRequest
+      .body(requestForm)
+      .post(uri"$BASE_URL/$command")
+      .send(backend)
+  }
+
+  def getImageReplyMarkup(image: Map[String, AttributeValue]) {}
+
+  def isPhoto(imageUrl: String): Boolean = {
+    isImageFormat(imageUrl, ".jpg") || isImageFormat(imageUrl, ".jpeg")
+  }
+
+  def isImageFormat(imageUrl: String, format: String): Boolean = {
+    imageUrl.endsWith(format)
+  }
+
+  def getCaption(image: Map[String, AttributeValue]): String = {
+    image.get("title").get.getS() + "\n" + image.get("publish_date").get.getS()
+  }
+
 }
 
 case class Update(
